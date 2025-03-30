06ca2fb907af3710ac9d53014138220c1e279f60
Dizhman <160435612+Dizhman@users.noreply.github.com>
2025-03-30 03:54:41 +0200

    открытие и закрытие формы

--- js/main.js
+++ js/main.js
@@ -13,3 +13,21 @@
 import { generatePhotos } from './data.js';
 
 const photoArray = generatePhotos();
+
+import { initFormValidation } from './form-validation.js';
+
+initFormValidation();
+
+import { initImageEditor, closeImageEditor } from './image-editor.js';
+
+// При открытии формы
+const openImageEditor = () => {
+  initImageEditor();
+  // ... другая логика инициализации
+};
+
+// При закрытии формы
+const onCloseButtonClick = () => {
+  closeImageEditor();
+  // ... другая логика закрытия
+};
